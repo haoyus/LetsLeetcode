4e105e488bcb6bf7c1c0cc8eed28bfdeeece2708
haoyus <haoyus1989@gmail.com>
2019-10-07 12:16:37 -0400

    fet: insert sort

---
+++ src/common.h
@@ -0,0 +1,8 @@
+#pragma once
+
+struct Object
+{
+    int x,y,vx,vy;
+
+    Object(int xx=0,int yy=0,int vvx=0,int vvy=0) : x(xx),y(yy),vx(vvx),vy(vvy) {};
+};
---
+++ src/insertSort.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include "common.h"
+
+void insertSort(Object* pObj, int N)
+{
+    for(int j=1;j<N;++j){
+        Object key = pObj[j];
+
+        int i = j-1;
+        while(i>=0 && pObj[i].y>key.y){
+            pObj[i+1] = pObj[i];//move
+            --i;
+        }
+        pObj[i+1] = key;//insert
+
+        for (int k = 0; k < N; ++k)
+        {
+            std::cout << pObj[k].y << " ";
+        }
+        std::cout << "\n";
+    }
+}
+
+int main()
+{
+    int num = 10;
+    Object* head= new Object[num];
+    Object* tmp = head;
+    for(int i=0;i<num;++i){
+        (tmp++)->y = num-i;
+        std::cout<<num-i<<" ";
+    } std::cout<<"\n";
+
+    insertSort(head,num);
+
+    Object *tmp2 = head;
+    for (int i = 0; i < num; ++i)
+    {
+        std::cout<<tmp2[i].y <<" ";
+    } std::cout << "\n";
+}
